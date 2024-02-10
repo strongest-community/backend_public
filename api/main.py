a103c23501e33d9da81cfbe758903cd3a934ada3
Yuki Fujita <uni22yu@gmail.com>
2024-02-10 19:32:26 +0900

    hotfix

--- api/main.py
+++ api/main.py
@@ -1,12 +1,21 @@
 """main module"""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from api.routers import sample,plans
+from api.routers import sample, plans
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Include the sample router
 app.include_router(sample.router)
 app.include_router(plans.router)
-
