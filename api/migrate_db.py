0419d36c7c9fddf383b4ef8c5ad00be468013c8f
Yuki Fujita <uni22yu@gmail.com>
2024-02-11 12:08:07 +0900

    add:seeds

--- api/migrate_db.py
+++ api/migrate_db.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import sessionmaker
 
 from dotenv import load_dotenv
 from api.models.models import Base
-import
+from api.seed import insert_seed_data
+
 load_dotenv()
 DB_URL = os.getenv("DB_URL")
 engine = create_engine(DB_URL, echo=True)
@@ -20,21 +21,6 @@ def reset_database():
     Base.metadata.create_all(bind=engine)
 
 
-def insert_seed_data():
-    """Insert seed data into the database."""
-    session = Session()
-    # シードデータの挿入例
-    user1 = User(name="Alice", email="alice@example.com")
-    user2 = User(name="Bob", email="bob@example.com")
-
-    session.add(user1)
-    session.add(user2)
-
-    # ここに他のシードデータを挿入するコードを追加
-
-    session.commit()
-    session.close()
-
-
 if __name__ == "__main__":
     reset_database()
+    insert_seed_data(session=Session())
